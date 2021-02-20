686101f25a735807dd8f90581de2c6c5f6f4ae26
Erik Villegas <erdvillegas@gmail.com>
2021-02-20 02:39:55 -0600

    Usando Redux

--- 02-redux-app/src/app/app.component.ts
+++ 02-redux-app/src/app/app.component.ts
@@ -1,5 +1,11 @@
 import { ThrowStmt } from '@angular/compiler';
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import * as actions from './contador/contador.actions';
+
+interface AppState {
+  contador: number;
+}
 
 @Component({
   selector: 'app-root',
@@ -11,15 +17,29 @@ export class AppComponent {
   contador: number;
   title = 'redux-app';
 
-  constructor() {
-    this.contador = 10;
+  constructor(private store: Store<AppState>) {
+    // this.contador = 10;
+
+    this.store.subscribe(state => {
+      console.log({ state });
+      this.contador = state.contador;
+    });
+    
   }
 
+  // incrementar() {
+  //   this.contador += 1;
+  // }
+
+  // decrementar() {
+  //   this.contador -= 1;
+  // }
+
   incrementar() {
-    this.contador += 1;
+    this.store.dispatch(actions.incrementar());
   }
 
   decrementar() {
-    this.contador -= 1;
+    this.store.dispatch(actions.decrementar());
   }
 }

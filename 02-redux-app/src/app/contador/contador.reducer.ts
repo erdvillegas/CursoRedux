cf818ff0a95f2b3cd303c061f1d0e33d7b8d36e0
Erik Villegas <erdvillegas@gmail.com>
2021-02-21 02:21:36 -0600

    Ajustes finales ngrx

--- 02-redux-app/src/app/contador/contador.reducer.ts
+++ 02-redux-app/src/app/contador/contador.reducer.ts
@@ -1,33 +1,15 @@
 import { createReducer, on } from "@ngrx/store";
-import { incrementar, decrementar, reset, multiplicar, dividir } from './contador.actions';
-
-
-// export function contadorReducer(state: number = 10, action: Action) {
-
-//   switch (action.type) {
-
-//     case incrementar.type:
-//       return state + 1;
-    
-//     case decrementar.type:
-//       return state - 1;
-    
-//     default:
-//       return state;
-//   }
-  
-// }
-
+import * as actions from './contador.actions';
 
 export const initialState = 0;
 
 const _contadorReducer = createReducer(
   initialState,
-  on(incrementar, state => state + 1),
-  on(decrementar, state => state - 1),
-  on(multiplicar, (state, { numero }) => state * numero),
-  on(dividir, (state, { numero }) => state / numero),
-  on(reset, state => 0)
+  on(actions.incrementar, state => state + 1),
+  on(actions.decrementar, state => state - 1),
+  on(actions.multiplicar, (state, { numero }) => state * numero),
+  on(actions.dividir, (state, { numero }) => state / numero),
+  on(actions.reset, state => initialState)
 );
 
 export function contadorReducer(state, action) {
